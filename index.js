9cc364dfb37f232ea2780973a84007b6304a6642
Grady253 <grady.andre2@gmail.com>
2023-03-02 14:53:54 -0800

    Pushing code to github.

---
+++ index.js
@@ -0,0 +1,17 @@
+let loginForm = document.getElementById('form');
+
+    
+loginForm.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    let userEmail = document.getElementById('email');
+
+    if (userEmail.value === "" ){
+        alert('User Email required.');
+    }else{
+        alert('User email successfully submitted.');
+        console.log(` ${userEmail.value} want's a notification when the application is finished.`);
+    }
+
+    userEmail.value = "";
+});
